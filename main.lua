4ba6a882566ffa64d12b8b18549ddab6f5f90daf
grow-a-garden <johnadrian139@gmail.com>
2025-07-21 02:42:31 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/BPzDwcrF"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/grow-a-garden-ctrl/EggDetectorV2/refs/heads/main/main.lua"))();
